cffcd9039ff9e107817974ae32a8860fb0ddccbf
Mikhail Rohacevich <mrohacevich@yandex.ru>
2026-04-03 12:32:41 +0300

    feat: implement subjects route and add cors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
+import cors from "cors";
 import express from "express";
+
+import subjectsRouter from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
 
+if (!process.env.FRONTEND_URL)
+	throw new Error("FRONTEND_URL is not set in .env file");
+
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL, // React app URL
+		methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+		credentials: true, // allow cookies
+	}),
+);
+
 app.use(express.json());
+
+app.use("/api/subjects", subjectsRouter);
 
 app.get("/", (req, res) => {
 	res.send("Backend server is running!");
-});
-
-app.listen(PORT, () => {
-	console.log(`Server running at http://localhost:${PORT}`);
 });
